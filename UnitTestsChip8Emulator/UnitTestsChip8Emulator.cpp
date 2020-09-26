f0d943507c5038c3a858231f9336c4d5643147e2
Joe Lee <joe.lee5@btinternet.com>
2020-09-27 00:39:58 +0100

    Add Chip8_Register Tests to UnitTestChip8Emulator
    Add unit tests to test the functionality of all of the CHIP-8 registers
    I, PC, SP, ST, DT and all V registers.

--- UnitTestsChip8Emulator/UnitTestsChip8Emulator.cpp
+++ UnitTestsChip8Emulator/UnitTestsChip8Emulator.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include"../Chip8Emulator/config.h"
 #include"../Chip8Emulator/Chip8_Stack.h"
 #include"../Chip8Emulator/Chip8_Registers.h"
 
@@ -10,11 +11,60 @@ namespace UnitTestsChip8Emulator
 {
 	TEST_CLASS(UnitTestsChip8_Registers)
 	{
-	public:
+		private:
+			Chip8_Registers registers;
+		public:
+			TEST_METHOD(V_Registers)
+			{
+				for (int i = 0; i < NUMBER_OF_V_REGISTERS; i++)
+				{
+					registers.SetVRegister(i, i);
+				}
 
-		TEST_METHOD(TestMethod1)
-		{
-		}
+				for (int i = 0; i < NUMBER_OF_V_REGISTERS; i++)
+				{
+					Assert().IsTrue(registers.GetVRegister(i) == i);
+				}
+			}
+
+			TEST_METHOD(I_Register)
+			{
+				registers.SetIRegister(0x3000);
+				Assert().IsTrue(registers.GetIRegister() == 0x3000);
+			}
+
+			TEST_METHOD(DT_Register)
+			{
+				registers.SetDTRegister(100);
+				Assert().IsTrue(registers.GetDTRegister() == 100);
+				registers.DecrementDTRegister();
+				Assert().IsTrue(registers.GetDTRegister() == 99);
+			}
+
+			TEST_METHOD(ST_Register)
+			{
+				registers.SetSTRegister(100);
+				Assert().IsTrue(registers.GetSTRegister() == 100);
+				registers.DecrementSTRegister();
+				Assert().IsTrue(registers.GetSTRegister() == 99);
+			}
+
+			TEST_METHOD(PC_Resgister)
+			{
+				registers.SetPCRegister(0x200);
+				Assert().IsTrue(registers.GetPCRegister() == 0x200);
+				registers.IncrementPCRegister();
+				Assert().IsTrue(registers.GetPCRegister() == (0x200 + INSTRUCTION_LENGTH));
+			}
+
+			TEST_METHOD(SP_Register)
+			{
+				Assert().IsTrue(registers.GetSPRegister() == 0);
+				registers.IncrementSPRegister();
+				Assert().IsTrue(registers.GetSPRegister() == 1);
+				registers.DecrementSPRegister();
+				Assert().IsTrue(registers.GetSPRegister() == 0);
+			}
 	};
 
 	TEST_CLASS(UnitTestsChip8_Stack)
@@ -23,13 +73,13 @@ namespace UnitTestsChip8Emulator
 			Chip8_Stack stack;
 			Chip8_Registers registers;
 		public:
-		TEST_METHOD(PushPop)
-		{
-			stack.Push(&registers, 10);
-			Assert().IsTrue(registers.GetSPRegister() == 1);
-			Assert().IsTrue(stack.Pop(&registers) == 10);
-			Assert().IsTrue(registers.GetSPRegister() == 0);
-		}
+			TEST_METHOD(PushPop)
+			{
+				stack.Push(&registers, 10);
+				Assert().IsTrue(registers.GetSPRegister() == 1);
+				Assert().IsTrue(stack.Pop(&registers) == 10);
+				Assert().IsTrue(registers.GetSPRegister() == 0);
+			}
 	};
 	TEST_CLASS(UnitTestsChip8_Memory)
 	{
