7e2799398150d1956791d0aebf9ef7febb4ed3a2
Joe Lee <joe.lee5@btinternet.com>
2020-09-26 20:35:42 +0100

    Complete Extended Opcodes for 0x8000
    The last of the opcodes for 0x8000 opcodes were completed and tested
    manually, using very masic observations. Full testing of the opcodes
    will be needed once all opcodes are implemented.
    
    Chip8_Stack debug messages where updated to display the index values
    are pushed/ popped to as an int not a char, this way they show the
    shack index correctly.

--- Chip8Emulator/Chip8.cpp
+++ Chip8Emulator/Chip8.cpp
@@ -130,23 +130,32 @@ void Chip8::exectue0x8000(unsigned short opcode, unsigned char x, unsigned char 
 		case 0x04:
 			std::cout << "ADD Vx Vy" << std::endl;
 			tmpL = registers.GetVRegister(x) + registers.GetVRegister(y);
-			if (tmpL > 255) registers.SetVRegister(0x0F, 0x01);
+			if (tmpL > 255) registers.SetVRegister(0x0F, 1);
 			registers.SetVRegister(x, tmpL);
 			break;
 
 		case 0x05:
 			std::cout << "SUB Vx Vy" << std::endl;
 			tmpL = registers.GetVRegister(x) - registers.GetVRegister(y);
-			if(registers.GetVRegister(x) > registers.GetVRegister(y)) registers.SetVRegister(0x0F, 0x01);
+			if(registers.GetVRegister(x) > registers.GetVRegister(y)) registers.SetVRegister(0x0F, 1);
 			break;
 
 		case 0x06:
+			std::cout << "SHR Vx {, Vy}" << std::endl;
+			registers.SetVRegister(0x0F, (registers.GetVRegister(x) & 0x01));
+			registers.SetVRegister(x, (registers.GetVRegister(x) / 2));
 			break;
 
 		case 0x07:
+			std::cout << "SUBN Vx, Vy" << std::endl;
+			if (registers.GetVRegister(y) > registers.GetVRegister(x)) registers.SetVRegister(0x0F, 1);
+			registers.SetVRegister(x, (registers.GetVRegister(y) - registers.GetVRegister(x)));
 			break;
 
 		case 0x0E:
+			std::cout << "SHL Vx {, Vy}" << std::endl;
+			registers.SetVRegister(0x0F, (registers.GetVRegister(x) & 0x100000000));
+			registers.SetVRegister(x, (registers.GetVRegister(x) * 2));
 			break;
 	}
 }
--- Chip8Emulator/Chip8_Stack.cpp
+++ Chip8Emulator/Chip8_Stack.cpp
@@ -8,7 +8,7 @@ void Chip8_Stack::Push(Chip8_Registers* registers, unsigned short value)
 
     if (DEBUG_MODE == 1)
     {
-        std::cout << "DEBUG: Pushing " << value << " to stack index " << registers->GetSPRegister() << std::endl;
+        std::cout << "DEBUG: Pushing " << value << " to stack index " << (int)registers->GetSPRegister() << std::endl;
     }
 
     stack[registers->GetSPRegister()] = value;
@@ -25,7 +25,7 @@ unsigned short Chip8_Stack::Pop(Chip8_Registers* registers)
 
     if (DEBUG_MODE == 1)
     {
-        std::cout << "DEBUG: Popping value from stack index " << registers->GetSPRegister() << std::endl;
+        std::cout << "DEBUG: Popping value from stack index " << (int)registers->GetSPRegister() << std::endl;
     }
 
     return stack[registers->GetSPRegister()];
